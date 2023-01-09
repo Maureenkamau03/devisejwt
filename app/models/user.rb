8fef7798a76c69cd2e112c4d6216c38cfed2c3ee
Maureenkamau03 <mkaymaureen@gmail.com>
2023-01-09 11:27:48 +0300

    secret key confoguration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   include Devise::JWT::RevocationStrategies::JTIMatcher
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
-         def jwt_payload
-          super
-        end
+  def jwt_payload
+    super
+  end
+  
 end
